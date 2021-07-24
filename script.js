322f2e64f9764f65c9e8d2122414a06d51361031
erindzs <edijsrindzs@gmail.com>
2021-07-24 17:34:54 +0300

    html css ready

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-let email = document.getElementById("email");
-let form = document.getElementById("form");
-let errors = document.getElementById("error");
-let checkbox = document.getElementById("checkbox");
-let pattern = /^[^ ]+@[^ ]+\/[a-z]{2,3}$/;
-
-form.addEventListener("submit", (e) => {
-    let messages = []
-
-    if(email.value === '' || email.value == null){
-        messages.push("Email address is required")
-    }
-
-    if (messages.length > 0){
-        e.preventDefault();
-        errors.innerText = messages.join(", ");
-    }
-
-    
-});
 
 
 ///^[^ ]+@[^ ]+\/[a-z]{2,3}$*/
